67480ad6b3a7baa5966277cfc8be9f4e789f2522
Danica Pamoleras <danicagpamoleras@iskolarngbayan.pup.edu.ph>
2021-06-14 22:09:02 +0800

    fixed conditions of operands and buttons

--- Form1.cs
+++ Form1.cs
@@ -35,7 +35,7 @@ namespace OOP_Calculator
        
         private void button_0_Click(object sender, EventArgs e)
         {
-            if (TextB1.Text == "0")
+            if (TextB1.Text == "0" || TextB1.Text == "NA")
             {
                 TextB1.Text = "0";
             }
@@ -47,7 +47,7 @@ namespace OOP_Calculator
 
         private void button_1_Click(object sender, EventArgs e)
         {
-            if (TextB1.Text=="0")
+            if (TextB1.Text=="0"||TextB1.Text=="NA")
             {
                 TextB1.Text = "1";
             }
@@ -59,7 +59,7 @@ namespace OOP_Calculator
 
         private void button_2_Click(object sender, EventArgs e)
         {
-            if (TextB1.Text == "0")
+            if (TextB1.Text == "0" || TextB1.Text == "NA")
             {
                 TextB1.Text = "2";
             }
@@ -71,7 +71,7 @@ namespace OOP_Calculator
 
         private void button_3_Click(object sender, EventArgs e)
         {
-            if (TextB1.Text == "0")
+            if (TextB1.Text == "0" || TextB1.Text == "NA")
             {
                 TextB1.Text = "3";
             }
@@ -83,7 +83,7 @@ namespace OOP_Calculator
 
         private void button_4_Click(object sender, EventArgs e)
         {
-            if (TextB1.Text == "0")
+            if (TextB1.Text == "0" || TextB1.Text == "NA")
             {
                 TextB1.Text = "4";
             }
@@ -95,7 +95,7 @@ namespace OOP_Calculator
 
         private void button_5_Click(object sender, EventArgs e)
         {
-            if (TextB1.Text == "0")
+            if (TextB1.Text == "0" || TextB1.Text == "NA")
             {
                 TextB1.Text = "5";
             }
@@ -107,7 +107,7 @@ namespace OOP_Calculator
 
         private void button_6_Click(object sender, EventArgs e)
         {
-            if (TextB1.Text == "0")
+            if (TextB1.Text == "0" || TextB1.Text == "NA")
             {
                 TextB1.Text = "6";
             }
@@ -119,7 +119,7 @@ namespace OOP_Calculator
 
         private void button_7_Click(object sender, EventArgs e)
         {
-            if (TextB1.Text == "0")
+            if (TextB1.Text == "0" || TextB1.Text == "NA")
             {
                 TextB1.Text = "7";
             }
@@ -131,7 +131,7 @@ namespace OOP_Calculator
 
         private void button_8_Click(object sender, EventArgs e)
         {
-            if (TextB1.Text == "0")
+            if (TextB1.Text == "0" || TextB1.Text == "NA")
             {
                 TextB1.Text = "8";
             }
@@ -143,7 +143,7 @@ namespace OOP_Calculator
 
         private void button_9_Click(object sender, EventArgs e)
         {
-            if (TextB1.Text == "0")
+            if (TextB1.Text == "0" || TextB1.Text == "NA")
             {
                 TextB1.Text = "9";
             }
